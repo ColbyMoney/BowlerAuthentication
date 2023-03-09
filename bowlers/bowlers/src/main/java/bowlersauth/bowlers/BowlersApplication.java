497eea2256f62d5ca93f6cec3d0894e748ec7343
Colby Money <122302338+ColbyMoney@users.noreply.github.com>
2023-03-08 18:51:11 -0600

    create initial spring boot backend

---
+++ bowlers/bowlers/src/main/java/bowlersauth/bowlers/BowlersApplication.java
@@ -0,0 +1,13 @@
+package bowlersauth.bowlers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BowlersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BowlersApplication.class, args);
+	}
+
+}
---
+++ bowlers/bowlers/src/test/java/bowlersauth/bowlers/BowlersApplicationTests.java
@@ -0,0 +1,13 @@
+package bowlersauth.bowlers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BowlersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
